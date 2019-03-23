a6f3b674092f1eef56aa27766a2b1048854f0896
Juan Pablo Capurro <juanpcapurro@gmail.com>
2019-03-23 15:37:01 -0300

    x220-cherrypick into master: qutebrowser video rapid hinting

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -30,3 +30,5 @@ c.tabs.background = True
 # Bindings for normal mode
 config.bind(';m', 'hint links userscript ~/.scripts/mpvappend')
 config.bind(';M', 'hint --rapid links userscript ~/.scripts/mpvappend')
+config.bind(';v', 'hint links userscript ~/.scripts/mpvideoappend')
+config.bind(';V', 'hint --rapid links userscript ~/.scripts/mpvideoappend')
