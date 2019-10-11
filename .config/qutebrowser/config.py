8a4f1528b2f1e80a8f8a0aee7b46c8f9c0d183f7
capu <capu@lore.lore>
2019-10-11 10:55:03 -0300

    x220-cherrypick into master: xmodmap autism, debian PATH, qutebrowser configs

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -13,15 +13,16 @@ for site in ['tinder.com']:
 # force a different user agent to fix whatsapp web issue 
 config.set('content.headers.user_agent', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.128 Safari/537.36", 'https://web.whatsapp.com/*')
 
-c.downloads.location.prompt = False
-c.tabs.background = True
+config.downloads.location.prompt = False
+config.tabs.background = True
+config.auto_save.session = True
 
 # editor setting
-c.editor.command = ["st","-f", "SourceCodePro:pixelsize=17", "-t", "i3_dropdown_quteditor","nvim", "-f", "{file}", "-c", "normal {line}G{column0}1"]
+config.editor.command = ["st", "-t", "i3_dropdown_quteditor","nvim", "-f", "{file}", "-c", "normal {line}G{column0}1"]
 
 # Bindings for normal mode
 config.bind('e', 'open-editor')
 config.bind(';m', 'hint links userscript ~/.scripts/mpvappend')
 config.bind(';M', 'hint --rapid links userscript ~/.scripts/mpvappend')
 # Bindings for insert mode
-config.bind('<Ctrl-e>', 'open-editor','insert')
+config.bind('<Ctrl-f>', 'open-editor','insert')
