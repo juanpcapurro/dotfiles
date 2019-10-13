35b7e25c251f9b42ad95ec4027cc3aa5a150797d
Juan Pablo Capurro <jpcapurro@atixlabs.com>
2019-10-13 20:22:12 -0300

    work-v310: cherrypick into master: qutebrowser config fixes

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -13,12 +13,12 @@ for site in ['tinder.com']:
 # force a different user agent to fix whatsapp web issue 
 config.set('content.headers.user_agent', "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.128 Safari/537.36", 'https://web.whatsapp.com/*')
 
-config.downloads.location.prompt = False
-config.tabs.background = True
-config.auto_save.session = True
+c.downloads.location.prompt = False
+c.tabs.background = True
+c.auto_save.session = True
 
 # editor setting
-config.editor.command = ["st", "-t", "i3_dropdown_quteditor","nvim", "-f", "{file}", "-c", "normal {line}G{column0}1"]
+c.editor.command = ["st", "-t", "i3_dropdown_quteditor","nvim", "-f", "{file}", "-c", "normal {line}G{column0}1"]
 
 # Bindings for normal mode
 config.bind('e', 'open-editor')
