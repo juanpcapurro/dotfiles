37ba8967bf7ffee6915a41db6a9752c7a59583ad
capu <hi@capu.tech>
2022-11-05 13:07:37 -0300

    update qutebrowser fun things

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,5 +1,5 @@
 config.set('content.javascript.enabled', False)
-js_enabled_sites=['windguru.cz', 'twitter.com', 'mercadolibre.com.ar', 'instagram.com', 'etherscan.io', 'github.com', 'gitlab.com', 'paypal.com', 'youtube.com', 'edensa.com.ar', 'airbnb.com', 'openstreetmap.org', 'duckduckgo.com', 'thingiverse.com', 'afip.gob.ar', 'old.reddit.com', 'piped.kavin.rocks']
+js_enabled_sites=['windguru.cz', 'twitter.com', 'mercadolibre.com.ar', 'instagram.com', 'etherscan.io', 'github.com', 'gitlab.com', 'paypal.com', 'youtube.com', 'edensa.com.ar', 'airbnb.com', 'openstreetmap.org', 'duckduckgo.com', 'thingiverse.com', 'afip.gob.ar', 'old.reddit.com', 'piped.kavin.rocks', 'mercadopago.com.ar']
 for site in js_enabled_sites :
     config.set('content.javascript.enabled', True, '*://*.'+site+'/*')
 
@@ -7,12 +7,17 @@ c.downloads.location.prompt = False
 c.tabs.background = True
 c.auto_save.session = True
 
+# config.set('content.proxy', 'socks://localhost:9050')
+config.set('content.user_stylesheets', '~/.config/qutebrowser/styles.css')
+
 # editor setting
 c.editor.command = ["st", "-t", "i3_dropdown_quteditor","nvim", "-f", "{file}", "-c", "normal {line}G{column0}1"]
 
 config.set('url.searchengines', {
     'DEFAULT': 'https://duckduckgo.com/?q={}',
-    'yt': 'https://piped.kavin.rocks/results?search_query={}',
+    'yt': 'https://invidious.weblibre.org/search?q={}',
+    'lgf': 'https://libgen.is/fiction/?q={}',
+    'tv': 'https://thingiverse.com/search?q={}',
     'ml': 'https://listado.mercadolibre.com.ar/{}',
     'ens': 'https://etherscan.io/enslookup-search?search={}'
 })
@@ -26,6 +31,11 @@ config.bind('peak', 'open -- https://kovan.etherscan.io/address/{clipboard}')
 config.bind('Peak', 'open -- https://kovan.etherscan.io/address/{primary}')
 config.bind('petk', 'open -- https://kovan.etherscan.io/tx/{clipboard}')
 config.bind('Petk', 'open -- https://kovan.etherscan.io/tx/{primary}')
+
+config.bind('peag', 'open -- https://goerli.etherscan.io/address/{clipboard}')
+config.bind('Peag', 'open -- https://goerli.etherscan.io/address/{primary}')
+config.bind('petg', 'open -- https://goerli.etherscan.io/tx/{clipboard}')
+config.bind('Petg', 'open -- https://goerli.etherscan.io/tx/{primary}')
 
 config.bind('peap', 'open -- https://polygonscan.com/address/{clipboard}')
 config.bind('Peap', 'open -- https://polygonscan.com/address/{primary}')
