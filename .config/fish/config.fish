7fbef1644b16752ca26be124f5b625c5f93af8b8
capu <hi@capu.tech>
2022-11-19 19:02:46 -0300

    i3lock for most things +
    thingiverse search engine working
    non-dangerous alias
    glog alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,24 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    abbr fatvim nvim -u ~/.config/nvim/fat.vim
-    abbr xclipc xclip -selection clipboard
-    abbr config /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME
-    abbr gst git status
-    abbr gsh git show
-    abbr ga git add
-    abbr gf git fetch
-    abbr gd git diff
-    abbr gco git checkout
-    abbr gc git commit
-    abbr g git
+    abbr -a -g fatvim nvim -u ~/.config/nvim/fat.vim
+    abbr -a -g xclipc xclip -selection clipboard
+    abbr -a -g config /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME
+    abbr -a -g gst git status
+    abbr -a -g gsh git show
+    abbr -a -g ga git add
+    abbr -a -g gf git fetch
+    abbr -a -g gd git diff
+    abbr -a -g gco git checkout
+    abbr -a -g gc git commit
+    abbr -a -g g git
 
     function prompt_login --description 'display user name for the prompt'
         echo -n -s (set_color $fish_color_user) "$USER" (set_color normal) 
     end
 
 end
+
+fish_vi_key_bindings
 
 export EDITOR=nvim
 export PATH="$PATH:/home/capu/.foundry/bin"
